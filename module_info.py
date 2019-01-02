f97202c2d5a9e23aab1b3cbd23ef5e65e449f200
khamukkamu <khamukkamu@users.noreply.github.com>
2019-01-02 15:51:37 -0500

    First Commit

---
+++ module_info.py
@@ -0,0 +1,22 @@
+# Point export_dir to the folder you will be keeping your module
+# Make sure you use forward slashes (/) and NOT backward slashes (\)
+
+
+
+# Several possible variants for export_dir variable:
+
+# Relative path from module system unpacked to install dir
+export_dir = "C:/Users/prdeleon/Downloads/Modding/module_system_1166-overhaul/Deeds_Modsys/Compiled/"
+
+# Warband being installed to C:/Games
+#export_dir = "C:/Games/Mount&Blade Warband/Modules/Native/"
+
+# Warband being installed to default path on Windows XP or Vista
+#export_dir = "C:/Programs Files/Mount&Blade Warband/Modules/Native/"
+
+# Warband being installed to default path on Windows 7+
+#export_dir = "C:/Programs Files (x86)/Mount&Blade Warband/Modules/Native/"
+
+# Likely paths for Steam Warband installation:
+#export_dir = "C:/Programs Files/Steam/steamapps/common/Mount&Blade Warband/Modules/Native/"
+#export_dir = "C:/Programs Files (x86)/Steam/steamapps/common/Mount&Blade Warband/Modules/Native/"
